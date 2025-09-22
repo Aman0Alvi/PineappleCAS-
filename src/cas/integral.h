889f205e4bffdc41f3184859079acd409063e5c8
Aman0Alvi <aman0alvi@gmail.com>
2025-09-22 15:59:55 -0500

    Create integral.h

---
+++ src/cas/integral.h
@@ -0,0 +1,16 @@
+#ifndef INTEGRAL_H_
+#define INTEGRAL_H_
+
+#include "../ast.h"
+
+/*
+ * Computes the antiderivative (indefinite integral) of an expression with
+ * respect to a given variable. See integral.c for the implementation details.
+ *
+ * Parameters:
+ *  e          - the AST node to integrate; upon return it will contain the integral
+ *  respect_to - the variable with respect to which the integral is taken
+ */
+void antiderivative(pcas_ast_t *e, pcas_ast_t *respect_to);
+
+#endif /* INTEGRAL_H_ */
